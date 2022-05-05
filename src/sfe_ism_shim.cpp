8c7aa48db96afb11361e5289b6a328c9ce8ac219
Elias Santistevan <elias.santistevan@sparkfun.com>
2022-05-05 08:08:42 -0600

    Removes copied function

--- src/sfe_ism_shim.cpp
+++ src/sfe_ism_shim.cpp
@@ -1,25 +1,5 @@
 #include "sfe_ism_shim.h"
 
-
-////////////////////////////////////////////////////////////////////////////////////
-// setCommunicationBus()
-//
-// Method to set the bus object that is used to communicate with the device
-//
-//  Parameter    Description
-//  ---------    -----------------------------
-//  theBus       The communication bus object
-//  idBus        The id/address of the device on the bus
-
-void QwDevTMF882X::setCommunicationBus(QwI2C &theBus, uint8_t idBus)
-{
-    _i2cBus = &theBus;
-    _i2cAddress = idBus;
-}
-
-//////////////////////////////////////////////////////////////////////////////
-//
-//
 int32_t platform_write(void *handle uint8_t Reg, const uint8_t *Bufp, uint16_t len)
 {
 	return ((QwDevISM330DHCX*)handle->writeRegisterRegion(Reg,*Bufp, len));
