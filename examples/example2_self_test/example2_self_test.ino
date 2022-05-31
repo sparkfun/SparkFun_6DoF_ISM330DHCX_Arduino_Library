6100ba1f06f78235d5025096c3d7d498da597230
Elias Santistevan <elias.santistevan@sparkfun.com>
2022-05-31 08:26:38 -0600

    Adds second example (not complete)

---
+++ examples/example2_self_test/example2_self_test.ino
@@ -0,0 +1,69 @@
+#include <Wire.h>
+#include "SparkFun_ISM330DHCX.h"
+
+SparkFun_ISM330DHCX myISM; 
+sfe_ism_data_t accelData; 
+sfe_ism_data_t gyroData; 
+
+void setup(){
+
+	Wire.begin();
+
+	Serial.begin(115200);
+
+	if( !myISM.begin() ){
+		Serial.println("Did not begin.");
+		while(1);
+	}
+
+	myISM.deviceReset();
+
+	while( !myISM.getDeviceReset() ){ 
+		delay(1);
+	} 
+
+	
+	myISM.setDeviceConfig();
+	myISM.setBlockDataUpdate();
+	
+	myISM.setAccelDataRate(4);
+	myISM.setGyroDataRate(4);
+	myISM.setAccelFullScale(2); 
+	myISM.setGyroFullScale(2); 
+
+	myISM.setSlopeFilter(4);
+	myISM.setAccelFilterLP2();
+
+
+}
+
+void loop(){
+
+	if( myISM.checkStatus() ){
+		myISM.getAccel(&accelData);
+		myISM.getGyro(&gyroData);
+		Serial.print("Accelerometer: ");
+		Serial.print("X: ");
+		Serial.print(accelData.xData);
+		Serial.print(" ");
+		Serial.print("Y: ");
+		Serial.print(accelData.yData);
+		Serial.print(" ");
+		Serial.print("Z: ");
+		Serial.print(accelData.zData);
+		Serial.println(" ");
+		Serial.print("Gyroscope: ");
+		Serial.print("X: ");
+		Serial.print(gyroData.xData);
+		Serial.print(" ");
+		Serial.print("Y: ");
+		Serial.print(gyroData.yData);
+		Serial.print(" ");
+		Serial.print("Z: ");
+		Serial.print(gyroData.zData);
+		Serial.println(" ");
+	}
+
+	delay(100);
+}
+
