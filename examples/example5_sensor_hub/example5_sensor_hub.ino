f2875d9cb9a414d4d31b9082b4ea26d671a99c0d
Elias Santistevan <elias.santistevan@sparkfun.com>
2022-07-28 10:33:59 -0600

    Organizes sensor hub example

--- examples/example5_sensor_hub/example5_sensor_hub.ino
+++ examples/example5_sensor_hub/example5_sensor_hub.ino
@@ -2,8 +2,8 @@
 #include "SparkFun_ISM330DHCX.h"
 
 #define MAG_ADDR 0x30
-#define MAG_X_READ_REG 0x00
-#define MAG_X_READ_LEN 0x07
+#define MAG_READ_REG 0x00
+#define MAG_READ_LEN 0x07
 #define MAG_WRITE_REG 0x09
 #define MAG_WRITE_LEN 0x01
 
@@ -11,24 +11,22 @@ SparkFun_ISM330DHCX myISM;
 sfe_ism_data_t accelData; 
 sfe_ism_data_t gyroData; 
 
-sfe_hub_sensor_settings_t readsh;
+sfe_hub_sensor_settings_t readsh, writesh;
 
-readsh.address = MAG_ADDR; 
-readsh.subAddress = MAG_X_READ_REG; 
-readsh.length = MAG_X_READ_LEN; 
-
-sfe_hub_sensor_settings_t writesh;
-
-writesh.address = MAG_ADDR; 
-writesh.subAddress = MAG_WRITE_REG; 
-writesh.length = MAG_WRITE_LEN; 
-
-uint8_t shRawData[MAG_X_READ_LEN] = 0;
+uint8_t shRawData[MAG_READ_LEN] = {};
 uint32_t magXVal; 
 uint32_t magYVal; 
 uint32_t magZVal; 
 
 void setup(){
+
+	readsh.address = MAG_ADDR; 
+	readsh.subAddress = MAG_READ_REG; 
+	readsh.length = MAG_READ_LEN; 
+
+	writesh.address = MAG_ADDR; 
+	writesh.subAddress = MAG_WRITE_REG; 
+	writesh.length = MAG_WRITE_LEN; 
 
 	Wire.begin();
 
@@ -52,27 +50,50 @@ void setup(){
 	myISM.setDeviceConfig();
 	myISM.setBlockDataUpdate();
 
+	// ******
+	// Might need a soft reset of MMC
+	// Enable Passthrough mode, reset MMC and then
+	// Move on.......
+	// ******
+
+	// Set up write registers
+	myISM.setHubSensorWrite(&writesh);
+
+	// Set up read Registers
+	// Which sensor (0-3) and settings to apply to given sensor
+	myISM.setHubSensorRead(0, &readsh);
+
+	// Set the number of peripheral sensor to be read by the 6DoF
+	myISM.setNumberHubSensors(1);
+
+	// Enable pullup resistors on SDX/SCX
+	myISM.setHubPullUps(); 
 	// Output data rate for the external sensor
 	myISM.setHubODR(ISM_SH_ODR_52Hz);
-	// Which sensor (0-3) and settings to apply to sensor?
-	myISM.setHubSensor(0, &sensor_hub);
-	// Enable the ISM330DHCX as controller I2C
+
+	// Enable the 6DoF as a controller I2C
+	// All configurations to the sensor hub must occur while the controller I2C 
+	// bus is powered down.
 	myISM.enableSensorI2C(true);
-	delay(1);
-	myISM.setHubPullUps(); 
+
+	//Wait 300us
+	delayMicroseconds(350);
 	
-	// Apply ISM330DHCX settings
-	myISM.setAccelDataRate(ISM_XL_ODR_104Hz);
+	// Apply acceleromter settings
 	myISM.setAccelFullScale(ISM_4g); 
-
-	myISM.setGyroFullScale(ISM_500dps); 
-	myISM.setGyroDataRate(ISM_GY_ODR_104Hz);
-
 	myISM.setAccelFilterLP2();
 	myISM.setAccelSlopeFilter(ISM_LP_ODR_DIV_100);
 
+	// Apply gyroscope settings
+	myISM.setGyroFullScale(ISM_500dps); 
 	myISM.setGyroFilterLP1();
 	myISM.setGyroLP1Bandwidth(ISM_MEDIUM);
+
+	// Enable Sensors
+	myISM.setAccelDataRate(ISM_XL_ODR_104Hz);
+	myISM.setGyroDataRate(ISM_GY_ODR_104Hz);
+
+
 
 
 
@@ -81,8 +102,18 @@ void setup(){
 void loop(){
 
 	if( myISM.checkStatus() ){
+
 		myISM.getAccel(&accelData);
 		myISM.getGyro(&gyroData);
+		myISM.readPeripheralSensor(shRawData, (uint8_t)MAG_READ_LEN);
+
+		Serial.print("Test: ");
+		Serial.println(shRawData[0]);
+
+		magXVal = shRawData[0] | shRawData [1] | (shRawData[6] & 0x20); 
+		magYVal = shRawData[2] | shRawData [3] | (shRawData[6] & 0x20); 
+		magZVal = shRawData[4] | shRawData [5] | (shRawData[6] & 0x20); 
+
 		Serial.print("Accelerometer: ");
 		Serial.print("X: ");
 		Serial.print(accelData.xData);
@@ -103,23 +134,29 @@ void loop(){
 		Serial.print("Z: ");
 		Serial.print(gyroData.zData);
 		Serial.println(" ");
-		myISM.readPeripheralSensor(&shRawData, MAG_X_READ_LEN);
-		magXVal = shRawData[0] | shRawData [1] | (shRawData[6] & 0x20); 
-		magXVal = shRawData[2] | shRawData [3] | (shRawData[6] & 0x20); 
-		magXVal = shRawData[4] | shRawData [5] | (shRawData[6] & 0x20); 
+		Serial.print("Magnetometer: ");
+		Serial.print("X: ");
+		Serial.print(magXVal);
+		Serial.print(" ");
+		Serial.print("Y: ");
+		Serial.print(magYVal);
+		Serial.print(" ");
+		Serial.print("Z: ");
+		Serial.print(magZVal);
+		Serial.println(" ");
 	}
 
 	delay(100);
 }
 
 
-void writeControlBit(sfe_hub_sensor_settings_t *toWrite)
-{
-
-	myISM.enableSensorI2C(true);
-
-	myISM.setAccelDataRate(ISM_XL_ODR_OFF);
-	myISM.setGyroDataRate(ISM_GY_ODR_OFF);
-	myISM.setHubSensorWrite(&toWrite);
-
-}
+//void writeControlBit(sfe_hub_sensor_settings_t *toWrite)
+//{
+//
+//	myISM.enableSensorI2C(true);
+//
+//	myISM.setAccelDataRate(ISM_XL_ODR_OFF);
+//	myISM.setGyroDataRate(ISM_GY_ODR_OFF);
+//	myISM.setHubSensorWrite(&toWrite);
+//
+//}
