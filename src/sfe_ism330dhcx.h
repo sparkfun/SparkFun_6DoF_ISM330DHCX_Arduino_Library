709da3c9c5c9b76546b0eaee8280200137df65c6
Jimmy van den Berg <vandenberg.jimmy@gmail.com>
2022-12-09 14:08:00 +0100

    Change comments in sfe_ism330dhcx.h to doxygen style.

--- src/sfe_ism330dhcx.h
+++ src/sfe_ism330dhcx.h
@@ -31,151 +31,139 @@ struct sfe_hub_sensor_settings_t
 
 class QwDevISM330DHCX
 {
-	public: 
+public:
 
-		QwDevISM330DHCX() : _i2cAddress{0}, _cs{0} {};
-			
+	QwDevISM330DHCX() : _i2cAddress{0}, _cs{0} {};
 
-    ///////////////////////////////////////////////////////////////////////
-    // init()
-    //
-    // Called to init the system. Connects to the device and sets it up for 
-    // operation
+	/**
+	 * @brief      Called to init the system. Connects to the device and sets it
+	 *             up for operation
+	 *
+	 * @return     false on success
+	 */
+	bool init();
 
-    bool init();
+	/**
+	 * @brief      Determines if connected.
+	 *
+	 * @return     True if connected, False otherwise.
+	 */
+	bool isConnected(); // Checks if sensor ack's the I2C request
 
-    ///////////////////////////////////////////////////////////////////////
-    // isConnected()
-    //
-    //
-    //  Parameter   Description
-    //  ---------   -----------------------------
-    //  retval      true if device is connected, false if not connected
+	int32_t writeRegisterRegion(uint8_t reg, uint8_t *data, uint16_t length);
 
-    bool isConnected(); // Checks if sensor ack's the I2C request
+	/**
+	 * @brief      Reads a register region.
+	 *
+	 * @param[in]  reg     The register to read from
+	 * @param[ou]  data    The data
+	 * @param[in]  length  The length of the data to read
+	 *
+	 * @return     0 on success
+	 */
+	int32_t readRegisterRegion(uint8_t reg, uint8_t *data, uint16_t length);
 
-    int32_t writeRegisterRegion(uint8_t reg, uint8_t *data, uint16_t length);
+	/**
+	 * @brief      Sets the communication bus.
+	 *
+	 * @param      theBus      The Bus object to use
+	 * @param[in]  i2cAddress  The bus i2c address of the target device.
+	 */
+	void setCommunicationBus(sfe_ISM330DHCX::QwIDeviceBus &theBus, uint8_t i2cAddress);
+	void setCommunicationBus(sfe_ISM330DHCX::QwIDeviceBus &theBus);
 
-    //////////////////////////////////////////////////////////////////////////////////
-    // readRegisterRegion()
-    //
-    //
-    //  Parameter    Description
-    //  ---------    -----------------------------
-    //  reg          register to read from
-    //  data         Array to store data in
-    //  length       Length of the data to read
-    //  retval       -1 = error, 0 = success
+	bool setAccelFullScale(uint8_t val);
+	bool setGyroFullScale(uint8_t val);
+	uint8_t getAccelFullScale();
+	uint8_t getGyroFullScale();
+	uint8_t getUniqueId();
 
-    int32_t readRegisterRegion(uint8_t reg, uint8_t *data, uint16_t length);
+	// Linear, Angular, and Temp Data retrieval
+	int16_t getTemp();
+	bool getRawAccel(sfe_ism_raw_data_t* accelData);
+	bool getRawGyro(sfe_ism_raw_data_t* gyroData);
+	bool getAccel(sfe_ism_data_t* accelData);
+	bool getGyro(sfe_ism_data_t* gyroData);
 
-    //////////////////////////////////////////////////////////////////////////////////
-    // setCommunicationBus()
-    //
-    // Called to set the Communication Bus object to use
-    //
-    //  Parameter    Description
-    //  ---------    -----------------------------
-    //  theBus       The Bus object to use
-    //  idBus        The bus ID for the target device.
-    //
+	// General Settings
+	bool setDeviceConfig(bool enable = true);
+	bool deviceReset();
+	bool getDeviceReset();
+	bool setAccelSlopeFilter(uint8_t val);
+	bool setAccelFilterLP2(bool enable = true);
+	bool setGyroFilterLP1(bool enable = true);
+	bool setGyroLP1Bandwidth(uint8_t val);
+	bool setBlockDataUpdate(bool enable = true);
+	uint8_t getBlockDataUpdate();
+	bool setAccelDataRate(uint8_t rate);
+	bool setGyroDataRate(uint8_t rate);
+	bool enableTimestamp(bool enable = true);
+	bool resetTimestamp();
 
-		void setCommunicationBus(sfe_ISM330DHCX::QwIDeviceBus &theBus, uint8_t i2cAddress);
-		void setCommunicationBus(sfe_ISM330DHCX::QwIDeviceBus &theBus);
+	// Interrupt Settings
+	bool setAccelStatustoInt1(bool enable = true);
+	bool setAccelStatustoInt2(bool enable = true);
+	bool setGyroStatustoInt1(bool enable = true);
+	bool setGyroStatustoInt2(bool enable = true);
+	bool setIntNotification(uint8_t val);
+	bool setDataReadyMode(uint8_t val);
+	bool setPinMode(bool activeLow = true);
 
-		bool setAccelFullScale(uint8_t val);
-		bool setGyroFullScale(uint8_t val);
-		uint8_t getAccelFullScale();
-		uint8_t getGyroFullScale();
+	// FIFO Settings
+	bool setFifoWatermark(uint16_t val);
+	bool setFifoMode(uint8_t val);
+	bool setAccelFifoBatchSet(uint8_t val);
+	bool setGyroFifoBatchSet(uint8_t val);
+	bool setFifoTimestampDec(uint8_t val);
 
-		uint8_t getUniqueId();
+	// Sensor Hub Settings
+	bool setHubODR(uint8_t rate);
+	bool setHubSensorRead(uint8_t sensor, sfe_hub_sensor_settings_t* settings);
+	bool setHubSensorWrite(sfe_hub_sensor_settings_t* settings);
+	bool setNumberHubSensors(uint8_t numSensors);
+	bool enableSensorI2C(bool enable);
+	bool readPeripheralSensor(uint8_t* shReg, uint8_t len);
+	bool setHubWriteMode(uint8_t config);
+	bool readMMCMagnetometer(uint8_t* magData, uint8_t len);
+	bool setHubPassThrough(bool enable = true);
+	bool setHubFifoBatching(bool enable = true);
+	bool setHubPullUps(bool enable = true);
+	bool getHubStatus();
+	bool getExternalSensorNack(uint8_t sensor);
+	bool resetSensorHub();
 
-		// Linear, Angular, and Temp Data retrieval 
-		int16_t getTemp();
-		bool getRawAccel(sfe_ism_raw_data_t* accelData);
-		bool getRawGyro(sfe_ism_raw_data_t* gyroData);
-		bool getAccel(sfe_ism_data_t* accelData);
-		bool getGyro(sfe_ism_data_t* gyroData);
-
-		// General Settings
-		bool setDeviceConfig(bool enable = true);
-		bool deviceReset();
-		bool getDeviceReset();
-		bool setAccelSlopeFilter(uint8_t val);
-		bool setAccelFilterLP2(bool enable = true);
-		bool setGyroFilterLP1(bool enable = true);
-		bool setGyroLP1Bandwidth(uint8_t val);
-		bool setBlockDataUpdate(bool enable = true);
-		uint8_t getBlockDataUpdate();
-		bool setAccelDataRate(uint8_t rate);
-		bool setGyroDataRate(uint8_t rate);
-		bool enableTimestamp(bool enable = true);
-		bool resetTimestamp();
-
-		// Interrupt Settings
-		bool setAccelStatustoInt1(bool enable = true);
-		bool setAccelStatustoInt2(bool enable = true);
-		bool setGyroStatustoInt1(bool enable = true);
-		bool setGyroStatustoInt2(bool enable = true);
-		bool setIntNotification(uint8_t val);
-		bool setDataReadyMode(uint8_t val);
-		bool setPinMode(bool activeLow = true);
-		
-		// FIFO Settings
-		bool setFifoWatermark(uint16_t val);
-		bool setFifoMode(uint8_t val);
-		bool setAccelFifoBatchSet(uint8_t val);
-		bool setGyroFifoBatchSet(uint8_t val);
-		bool setFifoTimestampDec(uint8_t val);
-			
-		// Sensor Hub Settings
-		bool setHubODR(uint8_t rate);
-		bool setHubSensorRead(uint8_t sensor, sfe_hub_sensor_settings_t* settings);
-		bool setHubSensorWrite(sfe_hub_sensor_settings_t* settings);
-		bool setNumberHubSensors(uint8_t numSensors);
-		bool enableSensorI2C(bool enable);
-		bool readPeripheralSensor(uint8_t* shReg, uint8_t len);
-		bool setHubWriteMode(uint8_t config);
-		bool readMMCMagnetometer(uint8_t* magData, uint8_t len);
-		bool setHubPassThrough(bool enable = true);
-		bool setHubFifoBatching(bool enable = true);
-		bool setHubPullUps(bool enable = true);
-		bool getHubStatus();
-		bool getExternalSensorNack(uint8_t sensor);
-		bool resetSensorHub();
-
-		// Self Test
-		bool setAccelSelfTest(uint8_t val);
-		bool setGyroSelfTest(uint8_t val);
+	// Self Test
+	bool setAccelSelfTest(uint8_t val);
+	bool setGyroSelfTest(uint8_t val);
 
 
-		// Status
-		bool checkStatus();
-		bool checkAccelStatus();
-		bool checkGyroStatus();
-		bool checkTempStatus();
+	// Status
+	bool checkStatus();
+	bool checkAccelStatus();
+	bool checkGyroStatus();
+	bool checkTempStatus();
 
-		// Conversions
-		float convert2gToMg(int16_t data);
-		float convert4gToMg(int16_t data);
-		float convert8gToMg(int16_t data);
-		float convert16gToMg(int16_t data);
-		float convert125dpsToMdps(int16_t data);
-		float convert250dpsToMdps(int16_t data);
-		float convert500dpsToMdps(int16_t data);
-		float convert1000dpsToMdps(int16_t data);
-		float convert2000dpsToMdps(int16_t data);
-		float convert4000dpsToMdps(int16_t data);
-		float convertToCelsius(int16_t data);
+	// Conversions
+	float convert2gToMg(int16_t data);
+	float convert4gToMg(int16_t data);
+	float convert8gToMg(int16_t data);
+	float convert16gToMg(int16_t data);
+	float convert125dpsToMdps(int16_t data);
+	float convert250dpsToMdps(int16_t data);
+	float convert500dpsToMdps(int16_t data);
+	float convert1000dpsToMdps(int16_t data);
+	float convert2000dpsToMdps(int16_t data);
+	float convert4000dpsToMdps(int16_t data);
+	float convertToCelsius(int16_t data);
 
-	private: 
+private:
 
-		
-		sfe_ISM330DHCX::QwIDeviceBus *_sfeBus; 
-		uint8_t _i2cAddress;
-		uint8_t _cs;
-		stmdev_ctx_t sfe_dev; 
-		uint8_t fullScaleAccel = 0; //Powered down by default
-		uint8_t fullScaleGyro = 0;  //Powered down by default
+
+	sfe_ISM330DHCX::QwIDeviceBus *_sfeBus;
+	uint8_t _i2cAddress;
+	uint8_t _cs;
+	stmdev_ctx_t sfe_dev;
+	uint8_t fullScaleAccel = 0; //Powered down by default
+	uint8_t fullScaleGyro = 0;  //Powered down by default
 };
 
